53138ec16076f0c0963c11720079c61d05df88e1
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-01-23 13:02:51 -0700

    remved file from unwanted directory

--- RAG101/__init__.py
+++
@@ -1,7 +0,0 @@
-# Define a variable
-PACKAGE_NAME = 'Utility Package'
-INFO = 'This Package contains the Python scripts that will be used in iur RAG project.'
-
-def package_info():
-    print('Package Name: ', PACKAGE_NAME)
-    print('Package Info: ', INFO)

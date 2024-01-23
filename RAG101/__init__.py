d49df2a9f644dd75f819e097183f46475566044a
Ibrahim-Ola <ibrahimolalekana@u.boisestate.edu>
2024-01-23 13:01:12 -0700

    added the init script

---
+++ RAG101/__init__.py
@@ -0,0 +1,7 @@
+# Define a variable
+PACKAGE_NAME = 'Utility Package'
+INFO = 'This Package contains the Python scripts that will be used in iur RAG project.'
+
+def package_info():
+    print('Package Name: ', PACKAGE_NAME)
+    print('Package Info: ', INFO)
